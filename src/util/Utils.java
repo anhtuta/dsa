f46a7a57ec43f9095d4bb86c8960beba22a931d7
Ta Anh Tu <tu.taanh@ascendcorp.com>
2023-04-15 18:06:34 +0700

    Add Print slice

--- src/util/Utils.java
+++ src/util/Utils.java
@@ -8,4 +8,16 @@ public class Utils {
         }
         System.out.println(a[a.length - 1] + "]");
     }
+
+    public static void printArray(int[] a, int start, int end) {
+        if (start < 0 || end > a.length - 1) {
+            System.out.println("Invalid params");
+            return;
+        }
+        System.out.print("Array [");
+        for (int i = start; i <= end - 1; i++) {
+            System.out.print(a[i] + " ");
+        }
+        System.out.println(a[end] + "]");
+    }
 }
