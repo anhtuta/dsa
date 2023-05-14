f43be09954c70e7dfbbb1e079bbf5a6abf4d5600
anhtuta <anhtu51195@gmail.com>
2023-05-15 00:47:56 +0700

    Add comment, description

--- src/practice/leetcode/medium/dp/MinimumFallingPathSum_931.java
+++ src/practice/leetcode/medium/dp/MinimumFallingPathSum_931.java
@@ -1,5 +1,31 @@
 package practice.leetcode.medium.dp;
 
+/**
+ * https://leetcode.com/problems/minimum-falling-path-sum/
+ * 
+ * Given an n x n array of integers matrix, return the minimum sum of any falling path through
+ * matrix.
+ * 
+ * A falling path starts at any element in the first row and chooses the element in the next row
+ * that is either directly below or diagonally left/right. Specifically, the next element from
+ * position (row, col) will be (row + 1, col - 1), (row + 1, col), or (row + 1, col + 1).
+ * 
+ * Example 1:
+ * Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+ * Output: 13
+ * Explanation: There are two falling paths with a minimum sum as shown. (See image on leetcode)
+ * 
+ * Example 2:
+ * Input: matrix = [[-19,57],[-40,-5]]
+ * Output: -59
+ * Explanation: The falling path with a minimum sum is shown. (See image on leetcode)
+ * 
+ * Constraints:
+ * 
+ * n == matrix.length == matrix[i].length
+ * 1 <= n <= 100
+ * -100 <= matrix[i][j] <= 100
+ */
 public class MinimumFallingPathSum_931 {
     // memo[i][j] = minSum của ô a[i][j] = tổng bé nhất của falling path khi rơi từ hàng đầu tiên đến ô
     // a[i][j] (note: ở hàng đầu tiên, bắt đầu rơi từ ô nào ko quan trọng nhé)
